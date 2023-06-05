15dcb274b6ac6cc7aa5e6647c4c07d13613d5f58
Mordechai <mordwein77@gmail.com>
2023-06-04 20:04:18 -0400

    seventeenth homepage commit

--- client/src/Home/ UncontrolledExample.js
+++ client/src/Home/ UncontrolledExample.js
@@ -2,6 +2,8 @@ import Carousel from 'react-bootstrap/Carousel';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function UncontrolledExample() {
+  const isMobile = window.innerWidth <= 768;
+
   return (
     <Carousel style={{ width: '60rem', margin: 'auto'}}>
       <Carousel.Item>
@@ -9,7 +11,10 @@ function UncontrolledExample() {
           className="d-block w-100"
           src="https://static.euronews.com/articles/stories/06/07/67/98/1000x563_cmsv2_03bc89fa-4248-5ad8-8140-3c07bcb9d73c-6076798.jpg"
           alt=" Woolly Mammoth"
-          style={{ objectFit: 'cover', width: '100%', height: '28.5rem' }}
+          style={{ 
+            width: isMobile ? '100%' : 'auto',
+            height: isMobile ? 'auto' : '28.5rem',
+          }}
         />
         <Carousel.Caption>
           <h3 style={{fontWeight: 'bold', fontFamily: 'Montserrat', fontSize: '2rem'}}>Woolly Mammoth</h3>
@@ -20,7 +25,10 @@ function UncontrolledExample() {
           className="d-block w-100"
           src="https://images.newscientist.com/wp-content/uploads/2022/07/25151808/SEI_116388714.jpg?crop=4:3,smart&width=1200&height=900&upscale=true"
           alt="Tyrannosaurus rex "
-          style={{ objectFit: 'cover', width: '100%', height: '28.5rem' }}
+          style={{ 
+            width: isMobile ? '100%' : 'auto',
+            height: isMobile ? 'auto' : '28.5rem',
+          }}
         />
 
         <Carousel.Caption>
@@ -32,7 +40,10 @@ function UncontrolledExample() {
           className="d-block w-100"
           src="https://www.telegraph.co.uk/multimedia/archive/01117/sabre-tooth-tiger-_1117360c.jpg"
           alt="Saber-toothed Tiger"
-          style={{ objectFit: 'cover', width: '100%', height: '28.5rem' }}
+          style={{ 
+            width: isMobile ? '100%' : 'auto',
+            height: isMobile ? 'auto' : '28.5rem',
+          }}
         />
 
         <Carousel.Caption>
