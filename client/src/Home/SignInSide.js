9f86ffeaba8c98a0de3e492b64b79a867a63d73f
Mordechai <mordwein77@gmail.com>
2023-06-05 20:35:26 -0400

    fifth sign in page commit

--- client/src/Home/SignInSide.js
+++ client/src/Home/SignInSide.js
@@ -3,15 +3,13 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Helmet } from 'react-helmet';
 
 function Copyright(props) {
   return (
@@ -26,15 +24,15 @@ function Copyright(props) {
   );
 }
 
-// TODO remove, this demo shouldn't need to reset the theme.
+
 
 const defaultTheme = createTheme({
   palette: {
     primary: {
-      main: '#00000000', // Override the primary color to red
+      main: '#00000000', 
     },
     secondary: {
-      main: '#ff9800', // Override the secondary color to green
+      main: '#ff9800', 
     },
   },
 });
@@ -52,6 +50,9 @@ export default function SignInSide() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
+      <Helmet>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+      </Helmet> 
       <Grid container component="main" sx={{ height: '100vh' }}>
         <CssBaseline />
         <Grid
