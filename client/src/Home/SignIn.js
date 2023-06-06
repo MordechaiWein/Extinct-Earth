8e58be775523871dbbc854c13d580a65230fb7d5
Mordechai <mordwein77@gmail.com>
2023-06-06 18:49:08 -0400

    dynamic home page commit 17

--- client/src/Home/SignIn.js
+++ client/src/Home/SignIn.js
@@ -1,158 +1,21 @@
-// import * as React from 'react';
-// import Avatar from '@mui/material/Avatar';
-// import Button from '@mui/material/Button';
-// import CssBaseline from '@mui/material/CssBaseline';
-// import TextField from '@mui/material/TextField';
-// import Link from '@mui/material/Link';
-// import Paper from '@mui/material/Paper';
-// import Box from '@mui/material/Box';
-// import Grid from '@mui/material/Grid';
-// import Typography from '@mui/material/Typography';
-// import { createTheme, ThemeProvider } from '@mui/material/styles';
-
-// function Copyright(props) {
-//   return (
-//     <Typography variant="body2" color="common.white" align="center" {...props}>
-//       {'Copyright © '}
-//       <Link color="inherit" href="">
-//         Extinct Earth
-//       </Link>{' '}
-//       {new Date().getFullYear()}
-//       {'.'}
-//     </Typography>
-//   );
-// }
-
-
-
-// const defaultTheme = createTheme({
-//   palette: {
-//     primary: {
-//       main: '#00000000', 
-//     },
-//     secondary: {
-//       main: '#ff9800', 
-//     },
-//   },
-// });
-
-
-// function SignIn() {
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-//     const data = new FormData(event.currentTarget);
-//     console.log({
-//       email: data.get('email'),
-//       password: data.get('password'),
-//     });
-//   };
-
-
-//   return (
-//     <ThemeProvider theme={defaultTheme}>
-//       <Grid container component="main" sx={{ height: '100vh'}}>
-//         <CssBaseline />
-//         <Grid
-//           item
-//           xs={false}
-//           sm={4}
-//           md={7}
-//           sx={{
-//             backgroundImage: 'url(https://wallpaperaccess.com/full/4457087.jpg)',
-//             backgroundRepeat: 'no-repeat',
-//             backgroundColor: (t) =>
-//               t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-//             backgroundSize: 'cover',
-//             backgroundPosition: 'center',
-//           }}
-//         />
-//         <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square sx={{backgroundColor: '#0f0f0f'}}>
-//           <Box
-//             sx={{
-//               my: 8,
-//               mx: 4,
-//               display: 'flex',
-//               flexDirection: 'column',
-//               alignItems: 'center'
-//             }}
-//           >
-//             <Avatar sx={{ m: 1, bgcolor: 'warning.light' }}>
-//             <img 
-//               width="38" 
-//               height="38" 
-//               src="https://img.icons8.com/glyph-neue/64/dinosaur.png"
-//               alt="dinosaur"
-//             />
-//             </Avatar>
-//             <Typography component="h1" variant="h4" sx={{color: 'white', fontWeight: 'bold'}}>
-//               Sign In
-//             </Typography>
-//             <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
-//               <TextField
-//                 margin="normal"
-//                 required
-//                 fullWidth
-//                 id="email"
-//                 placeholder='Username*'
-//                 name="email"
-//                 autoComplete="email"
-//                 autoFocus
-//                 sx={{backgroundColor: 'white', borderRadius: '0.3rem'}}
-//               />
-//               <TextField
-//                 margin="normal"
-//                 required
-//                 fullWidth
-//                 name="password"
-//                 placeholder='Password*'
-//                 type="password"
-//                 id="password"
-//                 autoComplete="current-password"
-//                 sx={{backgroundColor: 'white', borderRadius: '0.3rem'}}
-//               />
-           
-//               <Button
-//                 type="submit"
-//                 fullWidth
-//                 color='secondary'
-//                 variant="contained"
-//                 sx={{ mt: 3, mb: 2, fontWeight: 'bold', height: '3rem', color: 'white' }}
-//               >
-//                 Sign In
-//               </Button>
-//               <Copyright sx={{ mt: 5 }} />
-//             </Box>
-//           </Box>
-//         </Grid>
-//       </Grid>
-//     </ThemeProvider>
-//   );
-// }
-
-// export default SignIn
-
-
 import * as React from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 function Copyright(props) {
   return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+    <Typography variant="body2" color="common.white" align="center" {...props}>
       {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
+      <Link color="inherit" href="">
+        Extinct Earth
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
@@ -160,11 +23,21 @@ function Copyright(props) {
   );
 }
 
-// TODO remove, this demo shouldn't need to reset the theme.
 
-const defaultTheme = createTheme();
 
-export default function SignIn() {
+const defaultTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#00000000', 
+    },
+    secondary: {
+      main: '#ff9800', 
+    },
+  },
+});
+
+
+function SignIn() {
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
@@ -174,9 +47,10 @@ export default function SignIn() {
     });
   };
 
+
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Grid container component="main" sx={{ height: '100vh' }}>
+      <Grid container component="main" sx={{ height: '100vh'}}>
         <CssBaseline />
         <Grid
           item
@@ -184,7 +58,7 @@ export default function SignIn() {
           sm={4}
           md={7}
           sx={{
-            backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
+            backgroundImage: 'url(https://wallpaperaccess.com/full/4457087.jpg)',
             backgroundRepeat: 'no-repeat',
             backgroundColor: (t) =>
               t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
@@ -192,21 +66,26 @@ export default function SignIn() {
             backgroundPosition: 'center',
           }}
         />
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square sx={{backgroundColor: '#0f0f0f'}}>
           <Box
             sx={{
               my: 8,
               mx: 4,
               display: 'flex',
               flexDirection: 'column',
-              alignItems: 'center',
+              alignItems: 'center'
             }}
           >
-            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-              <LockOutlinedIcon />
+            <Avatar sx={{ m: 1, bgcolor: 'warning.light' }}>
+            <img 
+              width="38" 
+              height="38" 
+              src="https://img.icons8.com/glyph-neue/64/dinosaur.png"
+              alt="dinosaur"
+            />
             </Avatar>
-            <Typography component="h1" variant="h5">
-              Sign in
+            <Typography component="h1" variant="h4" sx={{color: 'white', fontWeight: 'bold'}}>
+              Sign In
             </Typography>
             <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
               <TextField
@@ -214,45 +93,33 @@ export default function SignIn() {
                 required
                 fullWidth
                 id="email"
-                label="Email Address"
+                placeholder='Username*'
                 name="email"
                 autoComplete="email"
                 autoFocus
+                sx={{backgroundColor: 'white', borderRadius: '0.3rem'}}
               />
               <TextField
                 margin="normal"
                 required
                 fullWidth
                 name="password"
-                label="Password"
+                placeholder='Password*'
                 type="password"
                 id="password"
                 autoComplete="current-password"
+                sx={{backgroundColor: 'white', borderRadius: '0.3rem'}}
               />
-              <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
-              />
+           
               <Button
                 type="submit"
                 fullWidth
+                color='secondary'
                 variant="contained"
-                sx={{ mt: 3, mb: 2 }}
+                sx={{ mt: 3, mb: 2, fontWeight: 'bold', height: '3rem', color: 'white' }}
               >
                 Sign In
               </Button>
-              <Grid container>
-                <Grid item xs>
-                  <Link href="#" variant="body2">
-                    Forgot password?
-                  </Link>
-                </Grid>
-                <Grid item>
-                  <Link href="#" variant="body2">
-                    {"Don't have an account? Sign Up"}
-                  </Link>
-                </Grid>
-              </Grid>
               <Copyright sx={{ mt: 5 }} />
             </Box>
           </Box>
@@ -261,3 +128,7 @@ export default function SignIn() {
     </ThemeProvider>
   );
 }
+
+export default SignIn
+
+
