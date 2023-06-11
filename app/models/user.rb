77dd093867769e5c1ab3fd12cf0c862d3879a859
Mordechai <mordwein77@gmail.com>
2023-06-11 15:25:19 -0400

    backend commit 8

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
     has_many :animals, through: :comments
 
     validates :username, :email_address, presence: true
-    vlaidates :username, uniqueness: true
+    validates :username, uniqueness: true
     validates :email_address, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
 end
