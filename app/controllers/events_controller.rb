0a469b57a38ccfd9a99d12f2f4260c146a0efa2f
Mordechai <mordwein77@gmail.com>
2023-06-11 13:27:09 -0400

    backend commit 2

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,30 +1,30 @@
 class EventsController < ApplicationController
     
-    def index
-        render json: Event.all
-    end
+    # def index
+    #     render json: Event.all
+    # end
 
-    def create
-        event = Event.create!(event_params)
-        render json: event, status: :created
-    end
+    # def create
+    #     event = Event.create!(event_params)
+    #     render json: event, status: :created
+    # end
 
-    def update
-        event = Event.find(params[:id])
-        event.update!(event_params)
-        render json: event
-    end
+    # def update
+    #     event = Event.find(params[:id])
+    #     event.update!(event_params)
+    #     render json: event
+    # end
 
-    def destroy
-        event = Event.find(params[:id])
-        event.destroy
-        render json: event
-    end
+    # def destroy
+    #     event = Event.find(params[:id])
+    #     event.destroy
+    #     render json: event
+    # end
 
-    private
+    # private
 
-    def event_params
-        params.permit(:name, :image, :description, :start_date, :end_date, :cause)
-    end
+    # def event_params
+    #     params.permit(:name, :image, :description, :start_date, :end_date, :cause)
+    # end
     
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,20 @@
 class UsersController < ApplicationController
 
-    def create
-        user = User.create!(user_params)
-        session[user_id] = user.id
-        render json: user, status: :created
-    end
+    # def create
+    #     user = User.create!(user_params)
+    #     session[user_id] = user.id
+    #     render json: user, status: :created
+    # end
 
-    def show
-        user = User.find(session[:user_id])
-        render json: user, status: :created
-    end
+    # def show
+    #     user = User.find(session[:user_id])
+    #     render json: user, status: :created
+    # end
 
-    private
+    # private
 
-    def user_params
-        params.permit(:username, :password, :password_confirmation, :email_address)
-    end
+    # def user_params
+    #     params.permit(:username, :password, :password_confirmation, :email_address)
+    # end
     
 end
