a5f25f592abdbd1cffd47e48457ecd7e3fb78ead
Mordechai <mordwein77@gmail.com>
2023-06-11 13:31:34 -0400

    backend commit 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  post '/signup', to: 'users#create'
-  get '/me', to: 'users#show'
-  post '/login', to: 'sessions#create'
-  delete, '/logout', to: 'sessions#destroy'
+  # post '/signup', to: 'users#create'
+  # get '/me', to: 'users#show'
+  # post '/login', to: 'sessions#create'
+  # delete, '/logout', to: 'sessions#destroy'
   resources :animals, only: [:index, :create, :update, :destroy]
-  resources :events, only: [:index, :create, :update, :destroy]
-  resources :comments, only: [:create, :update, :destroy]
+  # resources :events, only: [:index, :create, :update, :destroy]
+  # resources :comments, only: [:create, :update, :destroy]
   
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
