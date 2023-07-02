fa910cea7ec113cd21d78316255d238939c0e585
Mordechai <mordwein77@gmail.com>
2023-07-02 12:11:10 -0400

    final 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,6 @@ puts '🦖 Seeding deployed database...'
 
 User.destroy_all
 Comment.destroy_all
-Event.destroy_all
 Like.destroy_all
 User.create(username: 'Mordechai', password: 'ujiosazuki', password_confirmation: 'ujiosazuki', email_address: 'mordwein77@gmail.com', admin: true)
 
