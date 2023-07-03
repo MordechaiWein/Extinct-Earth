117e583d3a436025b999d49e657d3efc09c4158f
Mordechai <mordwein77@gmail.com>
2023-07-03 00:58:58 -0400

    new pssword

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ puts '🦖 Meteorite destroying all users comments and likes...'
 User.destroy_all
 Comment.destroy_all
 Like.destroy_all
-User.create(username: 'Mordechai', password: 'ujiosazuki', password_confirmation: 'ujiosazuki', email_address: 'mordwein77@gmail.com', admin: true)
+User.create(username: 'Mordechai', password: 'sensiblededuction', password_confirmation: 'sensiblededuction', email_address: 'mordwein77@gmail.com', admin: true)
 
 puts '✅ Done seeding!'
 
