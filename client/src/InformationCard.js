3b4fe7b416c29cd95c483d27f6a42af70f2587fd
Mordechai <mordwein77@gmail.com>
2023-07-25 17:08:19 -0400

    trying to get this spinner working

--- client/src/InformationCard.js
+++ client/src/InformationCard.js
@@ -30,7 +30,7 @@ function InformationCard() {
   
     return (
         <>
-            {!being && !being.image ? 
+            {!being ? 
                 (
                     <>
                         <Box 
