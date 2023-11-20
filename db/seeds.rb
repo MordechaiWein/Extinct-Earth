2095364f42b92cd54670f15ff469b881a59d1716
Mordechai <mordwein77@gmail.com>
2023-11-19 19:08:54 -0500

    removed error from seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,9 @@
-
 puts '🦖🦕💥 Meteorite destroying selected data...'
-puts "Destroying selected account..."
 
 puts "🔴"
 puts "🟡"
 puts "🟢"
 puts "🟣"
-
-moshe_gerstel = User.find_by(username: 'Tooky')
-moshe_gerstel.destroy
 
 puts '✅ Done seeding!'
 
